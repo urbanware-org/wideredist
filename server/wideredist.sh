85c0ce3a1a870251588119919128ada2a6486b9a
urbanware-org <github@urbanware.org>
2020-06-18 12:54:43 +0200

    Fix syntax error

--- server/wideredist.sh
+++ server/wideredist.sh
@@ -149,8 +149,7 @@ if [ -f "$script_dir/wideredist.upd" ]; then
 
     if [ -f "$script_dir/wideredist.conf.default" ] && \
        [ -f "$script_dir/wideredist.conf.new" ]; then
-            rm -f $script_dir/wideredist.conf.new
-        fi
+         rm -f $script_dir/wideredist.conf.new
     fi
 
     # Replace (overwrite to be precise) this script file on the fly and run
