829dea022d767e81d5ecb20fbc361403d1bf66fb
urbanware-org <github@urbanware.org>
2020-06-18 12:55:31 +0200

    Fix value check

--- server/wideredist.sh
+++ server/wideredist.sh
@@ -24,7 +24,8 @@ check_version() {
     rm -f $version_temp
     wget -U "$user_agent" "$version_url" -q -O $version_temp &>/dev/null
 
-    if [ $wideredist_update_check -eq 0 ]; then
+    if [ "$wideredist_update_check" = "" ] ||
+       [ $wideredist_update_check -eq 0 ]; then
         rm -f $definition_path/version.dat
         version_latest=""
         return
